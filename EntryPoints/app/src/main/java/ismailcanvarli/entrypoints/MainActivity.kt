e38c1130f718d836b6e4ae5a1efc2f3703b3c953
İsmail Can Varlı <ismailcanvarli@gmail.com>
2024-09-14 23:21:41 +0300

    Repeat previous implementation for Kotlin project entry point

--- EntryPoints/app/src/main/java/ismailcanvarli/entrypoints/MainActivity.kt
+++ EntryPoints/app/src/main/java/ismailcanvarli/entrypoints/MainActivity.kt
@@ -1,20 +1,79 @@
 package ismailcanvarli.entrypoints
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
+        Log.i("MainActivity", "onCreate1")
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        Log.i("MainActivity", "onCreate2")
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i("MainActivity", "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i("MainActivity", "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i("MainActivity", "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i("MainActivity", "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i("MainActivity", "onDestroy")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.i("MainActivity", "onRestart")
+    }
+
+    override fun onContentChanged() {
+        super.onContentChanged()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        Log.i("MainActivity", "onSaveInstanceState")
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        Log.i("MainActivity", "onRestoreInstanceState")
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+    }
+
+    override fun onUserLeaveHint() {
+        super.onUserLeaveHint()
+        Log.i("MainActivity", "onUserLeaveHint")
+    }
+
+    override fun onUserInteraction() {
+        super.onUserInteraction()
+        Log.i("MainActivity", "onUserInteraction")
+    }
+
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
     }
 }
