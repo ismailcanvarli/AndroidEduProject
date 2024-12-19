d23f94930d7508dcbc8fbee2a9fd9a705fa1c741
İsmail Can Varlı <ismailcanvarli@gmail.com>
2024-12-19 18:24:24 +0300

    Implement constraint layout for activity_constraint_layout.xml

---
+++ BasicComponentsAndEvents/app/src/main/java/com/ismailcanvarli/basiccomponentsandevents/ConstraintLayout.kt
@@ -0,0 +1,11 @@
+package com.ismailcanvarli.basiccomponentsandevents
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ConstraintLayout : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_constraint_layout)
+    }
+}
