8caf5e73279d7061925e57d12d56b659a57aedd4
İsmail Can Varlı <ismailcanvarli@gmail.com>
2024-12-20 16:32:43 +0300

    Compare tool text and android text

---
+++ BasicComponentsAndEvents/app/src/main/java/com/ismailcanvarli/basiccomponentsandevents/ToolTextAndHardcodedString.kt
@@ -0,0 +1,11 @@
+package com.ismailcanvarli.basiccomponentsandevents
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ToolTextAndHardcodedString : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tool_text_and_harcoded_string)
+    }
+}
