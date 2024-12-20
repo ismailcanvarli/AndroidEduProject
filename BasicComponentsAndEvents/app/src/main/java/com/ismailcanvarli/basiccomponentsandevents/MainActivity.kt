f17eab64b65c14337a80edd3d1da6fd7d7616347
İsmail Can Varlı <ismailcanvarli@gmail.com>
2024-12-20 16:38:21 +0300

    Add onClick event handling for push button in MainActivity

--- BasicComponentsAndEvents/app/src/main/java/com/ismailcanvarli/basiccomponentsandevents/MainActivity.kt
+++ BasicComponentsAndEvents/app/src/main/java/com/ismailcanvarli/basiccomponentsandevents/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.ismailcanvarli.basiccomponentsandevents
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    lateinit var btnPushButton: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initView()
+        initListener()
+    }
+
+    private fun initView() {
+        btnPushButton = findViewById<TextView>(R.id.push_btn)
+    }
+
+    private fun initListener() {
+        btnPushButton.setOnClickListener {
+            val intent = Intent(this, ToolTextAndHardcodedString::class.java)
+            startActivity(intent)
+        }
     }
 }
