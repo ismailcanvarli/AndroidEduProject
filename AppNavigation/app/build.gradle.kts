f076d88cc1c864924213bd6db775b92923202384
İsmail Can Varlı <ismailcanvarli@gmail.com>
2024-12-26 18:35:59 +0300

    feat: Add libraries related to Navigation

--- AppNavigation/app/build.gradle.kts
+++ AppNavigation/app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.ismailcanvarli.appnavigation"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.ismailcanvarli.appnavigation"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -42,7 +42,12 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    // Views/Fragments integration
+    implementation(libs.androidx.navigation.fragment)
+    implementation(libs.navigation.ui)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    // Testing Navigation
+    androidTestImplementation(libs.navigation.testing)
 }
