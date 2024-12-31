03154791c12f65b97e1b982fbe480d3b10775466
İsmail Can Varlı <ismailcanvarli@gmail.com>
2024-12-31 13:27:53 +0300

    feat: set up Navigation Graph and integrate MainActivity with DashboardFragment

--- AppNavigation/app/src/main/java/com/ismailcanvarli/appnavigation/DashboardFragment.kt
+++ AppNavigation/app/src/main/java/com/ismailcanvarli/appnavigation/DashboardFragment.kt
@@ -4,8 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
-
+import androidx.navigation.fragment.findNavController
 
 class DashboardFragment : Fragment() {
     override fun onCreateView(
@@ -13,5 +14,17 @@ class DashboardFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_dashboard, container, false)
+    }
+
+    // Fragment içerisindeki view'lar burada tanımlanır.
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // DashboardFragment içerisindeki btnOpenProfileFragment butonuna
+        // tıklandığında ProfileFragment'e geçiş yapılır.
+        val btnOpenProfileFragment = view.findViewById<Button>(R.id.btn_open_profile_fragment)
+        btnOpenProfileFragment.setOnClickListener {
+            findNavController().navigate(R.id.action_dashboardFragment_to_profileFragment)
+        }
     }
 }
--- AppNavigation/app/src/main/java/com/ismailcanvarli/appnavigation/MainActivity.kt
+++ AppNavigation/app/src/main/java/com/ismailcanvarli/appnavigation/MainActivity.kt
@@ -2,10 +2,24 @@ package com.ismailcanvarli.appnavigation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //getNavControllerViaFragment()
+    }
+
+    // biz en çok bunu kullanacağız.
+    fun getNavControllerViaView() {
+        val navHostFragmentView =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
+        navHostFragmentView.navController
+    }
+
+    override fun onResume() {
+        super.onResume()
+        getNavControllerViaView()
     }
 }
