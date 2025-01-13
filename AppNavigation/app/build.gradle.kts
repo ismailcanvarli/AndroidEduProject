a9eebef28b5ddb41207e77fb4c1fe66d0274af5d
İsmail Can Varlı <ismailcanvarli@gmail.com>
2025-01-13 19:47:23 +0300

    feat(navigation): add ktx dependencies and nested navigation graph

--- AppNavigation/app/build.gradle.kts
+++ AppNavigation/app/build.gradle.kts
@@ -44,6 +44,8 @@ dependencies {
     // Views/Fragments integration
     implementation(libs.androidx.navigation.fragment)
     implementation(libs.androidx.navigation.ui)
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
