e8717f7d2c5dc4041e4ac236f1c3f8a2f2c26400
İsmail Can Varlı <ismailcanvarli@gmail.com>
2025-01-16 12:44:45 +0300

    feat: add Navigation Fragment KTX and UI KTX dependencies

--- PaparaSample/app/build.gradle.kts
+++ PaparaSample/app/build.gradle.kts
@@ -45,4 +45,8 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
+    implementation(libs.androidx.drawerlayout)
 }
