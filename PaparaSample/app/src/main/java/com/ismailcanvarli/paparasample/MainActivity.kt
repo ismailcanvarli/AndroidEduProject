667780f304d9f69a5227b65c8868cfc58b204590
İsmail Can Varlı <ismailcanvarli@gmail.com>
2025-01-16 17:12:21 +0300

    feat(navigation): set up navigation with toolbar, bottom navigation, and drawer layout

--- PaparaSample/app/src/main/java/com/ismailcanvarli/paparasample/MainActivity.kt
+++ PaparaSample/app/src/main/java/com/ismailcanvarli/paparasample/MainActivity.kt
@@ -2,27 +2,58 @@ package com.ismailcanvarli.paparasample
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var toolbar: Toolbar
     private lateinit var drawerLayout: DrawerLayout
     private lateinit var navigationView: NavigationView
     private lateinit var bottomNavigationView: BottomNavigationView
     private lateinit var navHostFragment: NavHostFragment
 
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        initViews()
+        initEvents()
+    }
+
+    private fun initViews() {
+        toolbar = findViewById(R.id.toolbar)
         drawerLayout = findViewById(R.id.drawer_layout)
         navigationView = findViewById(R.id.navigation_view)
         bottomNavigationView = findViewById(R.id.bottom_nav)
-        navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
 
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.dashboardFragment,
+                R.id.qrDialogFragment,
+                R.id.moneyTransferDialogFragment,
+                R.id.paymentFragment,
+                R.id.paparaCardFragment,
+                R.id.notificationFragment,
+                R.id.searchAtmFragment,
+                R.id.withdrawDepositFragment
+            ), drawerLayout
+        )
     }
 
+    private fun initEvents() {
+        setSupportActionBar(toolbar)
+        setupActionBarWithNavController(navHostFragment.navController, appBarConfiguration)
+        navigationView.setupWithNavController(navHostFragment.navController)
+        bottomNavigationView.setupWithNavController(navHostFragment.navController)
+    }
 }
