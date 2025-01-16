1c7536df64d303fa628ba229026f1b892fbf8e57
İsmail Can Varlı <ismailcanvarli@gmail.com>
2025-01-16 13:07:21 +0300

    feat: add fragments and dialog fragments with their layouts

---
+++ PaparaSample/app/src/main/java/com/ismailcanvarli/paparasample/PaymentFragment.kt
@@ -0,0 +1,16 @@
+package com.ismailcanvarli.paparasample
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+
+class PaymentFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_payment, container, false)
+    }
+}
---
+++ PaparaSample/app/src/main/java/com/ismailcanvarli/paparasample/WithdrawDepositFragment.kt
@@ -0,0 +1,15 @@
+package com.ismailcanvarli.paparasample
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class WithdrawDepositFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_withdraw_deposit, container, false)
+    }
+}
