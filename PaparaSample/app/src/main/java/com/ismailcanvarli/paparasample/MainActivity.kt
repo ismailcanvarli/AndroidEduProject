841644d6aca49a7e55bfa64baa536c3db5540731
İsmail Can Varlı <ismailcanvarli@gmail.com>
2025-01-16 16:44:06 +0300

    feat(menu): add bottom navigation and drawer menus with nav graph integration

--- PaparaSample/app/src/main/java/com/ismailcanvarli/paparasample/MainActivity.kt
+++ PaparaSample/app/src/main/java/com/ismailcanvarli/paparasample/MainActivity.kt
@@ -2,10 +2,27 @@ package com.ismailcanvarli.paparasample
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.fragment.NavHostFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var navigationView: NavigationView
+    private lateinit var bottomNavigationView: BottomNavigationView
+    private lateinit var navHostFragment: NavHostFragment
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        drawerLayout = findViewById(R.id.drawer_layout)
+        navigationView = findViewById(R.id.navigation_view)
+        bottomNavigationView = findViewById(R.id.bottom_nav)
+        navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+
     }
+
 }
