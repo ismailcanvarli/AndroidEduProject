ee766fb6d6557438aac7bd25acb7fac9910985e2
İsmail Can Varlı <ismailcanvarli@gmail.com>
2025-01-17 11:51:07 +0300

    feat(menu): integrate with navigation controller

--- BottomBarApp/app/src/main/java/com/ismailcanvarli/bottombarapp/MainActivity.kt
+++ BottomBarApp/app/src/main/java/com/ismailcanvarli/bottombarapp/MainActivity.kt
@@ -2,10 +2,29 @@ package com.ismailcanvarli.bottombarapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var bottomNavigationView: BottomNavigationView
+    private lateinit var navHostFragment: NavHostFragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initViews()
+        initEvents()
+    }
+
+    private fun initViews() {
+        bottomNavigationView = findViewById(R.id.bottom_nav)
+        navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+    }
+
+    private fun initEvents() {
+        bottomNavigationView.setupWithNavController(navHostFragment.navController)
     }
 }
